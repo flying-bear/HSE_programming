8bb736385584346c83ac7f539bf2863e2d385db1
Flying Bear <Galka1999@gmail.com>
2017-11-30 11:57:39 +0300

    hw4-8 databased - not working

--- HSE_programming_2/hws/hw4-8/questionnaire.py
+++ HSE_programming_2/hws/hw4-8/questionnaire.py
@@ -8,13 +8,13 @@ from flask import Flask
 from flask import url_for, render_template, request, redirect
 
 import json
+import sqlite3
 
 from uuid import uuid4
 
 app = Flask(__name__)
 
 data = {}
-
 
 
 @app.route('/')
@@ -29,17 +29,26 @@ def index(): #анкета с социологическими полями --->
 
 @app.route('/', methods=['POST'])
 def process_userdata():
+    conn = sqlite3.connect('data.sqlite')
+    cur = conn.cursor()
     user_id = str(uuid4())
     userdata = {}
     userdata['userName'] = request.values['userName']
     userdata['userAge'] = request.values['userAge'],
     userdata['userSex'] = request.values['userSex'],
+    userdata['userLanguage'] = request.values['userLanguage'],
     userdata['userEducation'] = request.values['userEducation'],
     userdata['userYearsOfEducation'] = request.values['userYearsOfEducation'],
     userdata['userProfession'] = request.values['userProfession'],
     userdata['userBirthPlace'] = request.values['userBirthPlace'],
     userdata['userPlace'] = request.values['userPlace']
+    data[user_id] = {}
     data[user_id]['personal'] = userdata
+    cur.execute('INSERT INTO users (uid, Name, Age, Sex, Language, Education, YearsOfEducation, Profession, BirthPlace, Place) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                [user_id, data[user_id]['personal']['userName'], data[user_id]['personal']['userAge'], data[user_id]['personal']['userSex'],
+                data[user_id]['personal']['userLanguage'], data[user_id]['personal']['userEducation'], data[user_id]['personal']['userYearsOfEducation'],
+                data[user_id]['personal']['userProfession'], data[user_id]['personal']['userBirthPlace'], data[user_id]['personal']['userPlace']])
+    conn.commit()
     return redirect(url_for('form', uid=user_id))
 
 @app.route('/form') #анкета с социолингвистическими полями ---> записываться в файл
@@ -49,32 +58,45 @@ def form():
 
 @app.route('/form', methods=['POST']) #анкета с социолингвистическими полями ---> записываться в файл
 def process_form():
+    conn = sqlite3.connect('data.sqlite')
+    cur = conn.cursor()
     answers = request.form
     uid = request.args['uid'] 
     data[uid]['answers'] = answers
-    with open('results.json', 'a', encoding='utf-8') as f:
-        json.dump(data[uid], f, ensure_ascii = False, indent = 4)
-        f.write('\n,')
+    for i in range(1, 18):
+        cur.execute('INSERT INTO answers (uid, qid, answer) Values(?, ?, ?)',
+                    [uid, i, data[uid]['answers'][str(i)]])
+    conn.commit()
     return redirect(url_for('stats'))
 
 @app.route('/stats') # результаты в систематизированном виде
 def stats():
     number_of_participants = len(data)
+    mean_age = 0
+    for key in data:
+        mean_age += int(data[key]['userAge'])
+    mean_age = mean_age/number_of_participants
     ... #compute something
     return render_template('stats.html')
 
 
 @app.route('/json')
 def jsonify(): # json со всеми введенными на сайте данными
-    with open('results.json', 'r', encoding='utf-8') as f: ## перевести на БД
-        jsontext = json.loads(f.read().strip(',') + ']')
-    with open('templates/json.html', 'r', encoding='utf-8') as f:
-        currenttexet = f.read()
-        header = currenttexet.split('<span>')[0] + '<span>  '
-        footer = ' </span>' + currenttexet.split('</span>')[-1]
-    with open('templates/json.html', 'w', encoding='utf-8') as f:
-        f.write(header + jsontext + footer)
-    return render_template('json.html')
+    conn = sqlite3.connect('data.sqlite')
+    cur = conn.cursor()
+    jsontext = {}
+    cur.execute('SELECT uid FROM users')
+    uids = fetchone()
+    for u in uids:
+        jsontext[uid] = {}
+        cur.execute('SELECT * FROM users WHERE uid=?', u)
+        user_data = fetchone()
+        jsontext[uid]['personal'] = user_data
+        cur.execute('SELECT * FROM answers WHERE uid=?', u)
+        user_answers = fetchone()
+        jsontext[uid]['answers'] = user_answers
+    jsoned = json.loads(jsontext)
+    return jsoned
 
 @app.route('/search')
 def search(): # поиск

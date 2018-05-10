aac8ec7e7750c5a2e7b84d967661a0a37b93ef96
Flying Bear <Galka1999@gmail.com>
2018-05-10 21:42:39 +0300

    HW bot update (not working) tried to add find_form function

--- HSE_programming_2/hws/hw_bot/hw_bot.py
+++ HSE_programming_2/hws/hw_bot/hw_bot.py
@@ -5,6 +5,8 @@
 ##    Из этого списка вам нужен только список разных лемм разных частей речи, и затем нужно будет использовать функции parse и inflect.
 
 import json
+import os
+from random import choice
 from pymorphy2 import MorphAnalyzer
 morph = MorphAnalyzer()
 dictionary = {}
@@ -14,6 +16,11 @@ def get_file_lines(filename):
         lines = f.readlines()
     return lines
 
+def get_file_text(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        text = f.read()
+    return text
+
 
 def write_text_in_file(filename, text):
     with open(filename, 'w', encoding='utf-8') as f:
@@ -21,8 +28,12 @@ def write_text_in_file(filename, text):
 
     
 def analyze(form):
-    an = morph.parse(form)[0] ## Parse(word='любимая', tag=OpencorporaTag('ADJF,Subx,Qual femn,sing,nomn'), normal_form='любимый', 
+    a = morph.parse(form)[0] ## Parse(word='любимая', tag=OpencorporaTag('ADJF,Subx,Qual femn,sing,nomn'), normal_form='любимый', 
                               ## score=0.666666, methods_stack=((<DictionaryAnalyzer>, 'любимая', 367, 7),))
+    an = {}
+    an['tag'] = a.tag
+    an['normal'] = a.normal_form
+    an['word'] = a.word
     return an
 
 
@@ -30,15 +41,45 @@ def get_dictionary(lines):
     for line in lines:
         form = line.split()[-1].strip()
         analyzis = analyze(form)
-        tag = str(analyzis.tag)
-        normal = str(analyzis.normal_form)
-        dictionary[form] = {'tag':tag, normal:'normal'}
+        tag = analyzis['tag']
+        normal = analyzis['normal']
+        dictionary[form] = {'tag':tag, 'normal':normal}
     write_text_in_file('jsdict.json', json.dumps(dictionary, ensure_ascii = False))
                
 
+def find_form(grammar, d):
+    forms = []
+    for key in d:
+        if grammar == d[key]['tag']:
+            forms.append(grammar)
+        print(grammar)
+    if forms:
+        random_form = choice(forms)
+    else:
+        random_form = 'form_not_found'
+    return random_form
+
+
 def main():
-    lines = get_file_lines('dict_example.txt')
-    get_dictionary(lines)
+    if os.path.exists('jsdict.json'):
+        dictionary = json.loads(get_file_text('jsdict.json'))
+    else:
+        lines = get_file_lines('dict.txt')
+        get_dictionary(lines)
+    phrase = input('Введите предложение.  ')
+    words = phrase.split()
+    new_phrase = ''
+    for i in range(len(words)):
+        word = words[i]
+        g = analyze(word)['tag']
+        if i == 0:
+            new_phrase = find_form(g, dictionary).capitalize() + ' '
+        elif i == len(words) - 1:
+            new_phrase += find_form(g, dictionary)
+        else:
+            new_phrase += find_form(g, dictionary) + ' '
+    print(new_phrase)
+    
 
 
 if __name__ == '__main__':
